31ea72f5a452f485598318e0f71fb77ba2ca830c
Sarah Walters <swalters4925@gmail.com>
2017-01-30 11:57:03 -0500

    ADD: function for controlling individual cube face colors

---
+++ bin/run.sh
@@ -0,0 +1,1 @@
+g++ src/cube_state.cpp src/rubiks_cube_graphics.cpp -o out.o -lglut -lGL -lGLU && ./out.o
---
+++ src/cube_state.h
@@ -0,0 +1,6 @@
+#ifndef CUBE_STATE
+#define CUBE_STATE
+
+float** getColors(int x, int y, int z);
+
+#endif
