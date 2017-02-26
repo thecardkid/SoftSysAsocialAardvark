a1349c20fe9c246f5e78641fc0809429112d619d
Philip Seger <seger.philip@gmail.com>
2017-02-26 15:26:10 -0500

    ADD: timing for thread completion

--- src/Thread.c
+++ src/Thread.c
@@ -13,6 +13,7 @@ void create_threads(int state[6][3][3], int max_depth) {
 		memcpy(args->state, state, sizeof(state));
 		args->max_depth = max_depth;
 		printf("Starting thread #%d\n", t);
+		clock_t begin = clock();
 
 		rc = pthread_create(&threads[t], NULL, dfsSolve, args);
 		if (rc) {
@@ -20,10 +21,12 @@ void create_threads(int state[6][3][3], int max_depth) {
 			fprintf(stderr, "%s\n", "Thread creation failed");
 			exit(-1);
 		}
+		clock_t end = clock();
+		double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		printf("Time completed: %.6f", time_spent);
 	}
 
 	for (t=0; t<NUM_THREADS; t++) {
 		pthread_join(threads[t], NULL);
 	}
 }
-
