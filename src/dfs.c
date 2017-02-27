03041f758daa7e90a30f87730061832038bfc8d9
David Papp <david.papp@students.olin.edu>
2017-02-27 12:52:07 -0500

    ADD: comments

--- src/dfs.c
+++ src/dfs.c
@@ -7,11 +7,17 @@
 int*** solution;
 int max_depth;
 
-/**
- * @param args is the struct passed in by the calling function in Thread.c (check what it contains by looking in Enums.h). The args input is cast to a thread_struct type so we can access its attributes. This struct needs to be freed before the function returns
- * @return array of length 20 contain the moves to solve cube
- */
+/*
+Function is called by each thread. This function calls dfsSolveHelper() to find a
+solution. 
+@params: args is the struct passed in by the calling function in Thread.c 
+(check what it contains by looking in Enums.h). The args input is cast to a thread
+struct type so we can access its attributes. This struct needs to be freed before
+the function returns
 
+@return: a thread_struct struct containing an array of LetterNotations (Enums.h) 
+that represent the solution.
+*/
 void* dfs_solve(void* received_args) {
 	thread_struct *args = received_args;
 	thread_return_struct *return_args = malloc(sizeof(thread_return_struct));
@@ -44,14 +50,23 @@ void* dfs_solve(void* received_args) {
 	pthread_exit(return_args);
 }
 
-/* Recursively checks whether you can get from the current state to the solved state
-in N turns. Returns a char array of length 20 containing "None" if this is impossible.
-Returns a char array of length 20 containing the moves (in char format) to solve the cube.*/
+/*
+@params:
+int*** state: the starting state represented by a triple pointer.
+LetterNotation* moves: a pointer to an array of our enum type LetterNotation. The
+function modifies the contents of the array to the moves necessary for solving 
+the cube.
+int depth: this stores the current depth of the search. Pass 1 as an argument.
+
+@return: 1 if it finds a solution. 0 otherwise. It will also modify
+the contents of LetterNotation* moves regardless of whether a solution exists.
+*/
 
 int dfs_solve_helper(int*** state, LetterNotation* moves, int depth, int id) {
 	if (is_solved(state)) return 1;
 
 	if (depth == max_depth) return 0;
+
 
 	int i;
 	LetterNotation movement;
@@ -68,9 +83,13 @@ int dfs_solve_helper(int*** state, LetterNotation* moves, int depth, int id) {
 	return 0;
 }
 
+/*
+If two states differ at any value, return false. 
 
-/* If state 3D-matrices differ at any value, return false.*/
+@params: a state represented by a triple pointer 
 
+@return: 1 if two states are equal. 0 otherwise.
+*/
 int is_solved(int*** state) {
     int i, j, k;
     for (i = 0; i < 6; i++) {
--- src/dfs.h
+++ src/dfs.h
@@ -3,8 +3,24 @@
 
 #include "Shared.h"
 
+/*
+This is the function that should be called from each thread.  Returns a struct 
+containing an array of LetterNotation enums representing the moves required
+to solve the cube. This is then used by graphics to visually show the solution.
+*/
 void* dfs_solve(void* args);
+
+/*
+Recursively checks whether there is a solution from the given state to the solved
+state in some number of moves. This function is called by dfsSolve().
+*/
 int dfs_solve_helper(int*** state, LetterNotation* moves, int depth, int id);
+
+/*
+Checks whether a given state is equal to the solved state. Function takes
+a state represented by a triple pointer and compares it with a global. Returns
+1 if the two states are equal and 0 otherwise.
+*/
 int is_solved(int*** state);
 
 #endif
