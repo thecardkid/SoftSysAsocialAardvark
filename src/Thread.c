4b544592ed114651aa03dc8c82e8d0b2c899e29a
Philip Seger <seger.philip@gmail.com>
2017-02-27 12:19:15 -0500

    EDIT: create clock_t at the same time

--- src/Thread.c
+++ src/Thread.c
@@ -13,16 +13,17 @@ void create_threads(int state[6][3][3], int max_depth) {
 		memcpy(args->state, state, sizeof(state));
 		args->max_depth = max_depth;
 		printf("Starting thread #%d\n", t);
-		clock_t begin = clock();
+		clock_t begin, end;
 
+		begin = clock();
 		rc = pthread_create(&threads[t], NULL, dfsSolve, args);
 		if (rc) {
 			free(args);
 			fprintf(stderr, "%s\n", "Thread creation failed");
 			exit(-1);
 		}
-		clock_t end = clock();
-		double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		end = clock();
+		double time_spent = ((double)(end - begin)) / CLOCKS_PER_SEC;
 		printf("Thread #%d completed in %.6f seconds\n", t, time_spent);
 	}
 
